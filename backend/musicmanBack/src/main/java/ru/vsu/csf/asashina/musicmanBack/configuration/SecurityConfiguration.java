1d408bb431ecd1bf42ffbfba62e97cb520259bff
Arcturs <yaninastya2010@yandex.ru>
2023-05-21 22:01:27 +0300

    hotfix

--- backend/musicmanBack/src/main/java/ru/vsu/csf/asashina/musicmanBack/configuration/SecurityConfiguration.java
+++ backend/musicmanBack/src/main/java/ru/vsu/csf/asashina/musicmanBack/configuration/SecurityConfiguration.java
@@ -62,7 +62,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns()
+                .allowedOriginPatterns(frontUrl)
                 .allowedMethods("*");
     }
 }
